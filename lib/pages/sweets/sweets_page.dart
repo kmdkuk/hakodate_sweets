771115bdee4c999bc45bc2e827b4a15ceee5e078
kouki <kouworld0123@gmail.com>
2020-10-14 14:56:17 +0900

    add base pages

---
+++ lib/pages/sweets/sweets_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SweetsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text('SweetsPage'),
+      ),
+    );
+  }
+}
